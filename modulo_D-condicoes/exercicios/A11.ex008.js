710550e0ccf787498617f9246a4253de656b510e
helena <helenafurtado25@gmail.com>
2025-11-04 15:49:24 -0300

    Condições parte 1

---
+++ modulo_D-condicoes/exercicios/A11.ex008.js
@@ -0,0 +1,7 @@
+// testando extensao node js exec (ctrl + e) para executar
+var velocidade = 60.5 
+console.log(`a vleocidade do seu carro é ${velocidade} km/h`)
+if (velocidade > 60){
+    consolve.log('voce ultrapassou a velocidade permitida.MULTADO')
+}
+console.log('dirija semprecom seguranca')
